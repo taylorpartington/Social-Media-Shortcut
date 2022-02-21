6d016c9dfa8e2b7cacab4d1223bba0d083a3b2e7
Taylor Partington <taylorpartington@hotmail.com>
2022-02-21 16:02:46 -0700

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.taylorpartington.sociallmediashortcut {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.taylorpartington.sociallmediashortcut to javafx.fxml;
+    exports com.taylorpartington.sociallmediashortcut;
+}
