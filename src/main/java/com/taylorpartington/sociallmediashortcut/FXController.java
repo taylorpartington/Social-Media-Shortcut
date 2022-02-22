fe8a3872a546443a972803c88ea387a332830b7f
Taylor Partington <taylorpartington@hotmail.com>
2022-02-21 18:24:36 -0700

    Fixed GUI functionality *The "Open README" button opens the file

--- src/main/java/com/taylorpartington/sociallmediashortcut/FXController.java
+++ src/main/java/com/taylorpartington/sociallmediashortcut/FXController.java
@@ -3,6 +3,7 @@ package com.taylorpartington.sociallmediashortcut;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import java.awt.Desktop;
+import java.io.File;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -11,14 +12,13 @@ import java.net.URL;
 
 public class FXController {
     Desktop desk = Desktop.getDesktop();
-    URL path = FXController.class.getResource("README.md");
 
 
     @FXML
     private Label welcomeText;
 
     @FXML
-    protected void OpenReadme() {desk.open(path);}
+    protected void OpenReadme() throws IOException {desk.open(new File("README.md"));}
     @FXML
     protected void OpenLinkedIn() throws URISyntaxException, IOException {desk.browse(new URI("https://www.linkedin.com/"));}
     @FXML
